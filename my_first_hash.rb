10beaa9cdc44dea287d1bd0b307514243bf3e3d2
Yelstin Fernandes <yfernan2@u.rochester.edu>
2017-05-06 04:53:00 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,6 @@ def my_hash
   my_hash = {"Elite Quarterback" => "Eli Manning"}
 end
 
-
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
